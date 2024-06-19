ca069bc3dc9268a5c21369a8f5c035dd4c0f479a
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-19 20:46:51 +0500

    update

---
+++ task10.dart
@@ -0,0 +1,5 @@
+void main() {
+  print(
+      'Q.10: Write a Dart code that takes in a list of strings and removes any duplicate elements, returning a new list without duplicates. The order of elements in the new list should be the same as in the original list');
+  List<String> stringList = [];
+}
