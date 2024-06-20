068ef8b5e33a734dbdbe274703359a711cf942bc
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-20 23:00:40 +0500

    update

--- task13.dart
+++ task13.dart
@@ -1,6 +1,10 @@
+import 'dart:math';
+
 void main() {
   print(
       'Q.13: Implement a code that takes in a list of integers and returns a new list containing only the unique elements from the original list. The order of elements in the new list should be the same as in the original list');
-   
-
+  List<int> list = List<int>.generate(20, (index) => Random().nextInt(10));
+  print('The original list $list');
+  list = list.toSet().toList();
+  print('The list without duplicates $list');
 }
