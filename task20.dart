bfbd51796d55589e91cf56cf521d19c884563f66
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-22 00:45:51 +0500

    update

--- task20.dart
+++ task20.dart
@@ -1,3 +1,12 @@
 void main() {
-  print('');
+  print(
+      'Q.20: Create a map named "car" with the following key-value pairs: "brand" as "Toyota", "color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan and red in color. Print "Match" if both conditions are true, otherwise print "No match"');
+  Map<String, dynamic> car = {
+    'brand': 'Toyota',
+    'color': 'Red',
+    'isSedan': true,
+  };
+  car['color'] == 'Red' && car['isSedan']
+      ? print('Match')
+      : print(' Not a Match');
 }
