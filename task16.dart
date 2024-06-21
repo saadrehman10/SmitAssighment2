cd398754269931453f4ed191e7b04dc15ed3931c
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-21 20:00:30 +0500

    update

---
+++ task16.dart
@@ -0,0 +1,4 @@
+void main() {
+  print(
+      "Q.16: Implement a Dart code that uses the where() method to filter out odd numbers from a list of integers. The program should take in the original list as a parameter and print a new list containing only the even numbers.");
+}
