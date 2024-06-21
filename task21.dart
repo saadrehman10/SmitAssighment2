30110e36e2c81be63681cb4a4c211ec166dedc9d
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-22 00:52:05 +0500

    update

---
+++ task21.dart
@@ -0,0 +1,13 @@
+void main() {
+  print(
+      'Q.21: Given a map representing a user with keys "name", "isAdmin", and "isActive", write Dart code to check if the user is an active admin. If the user is both an admin and active, print "Active admin", otherwise print "Not an active admin".');
+  Map<String, dynamic> user = {
+    'name': 'Ahmed',
+    'isAdmin': true,
+    'isActive': true,
+  };
+
+  user['isAdmin'] && user['isActive']
+      ? print('Active admin')
+      : print('Not an active admin');
+}
---
+++ task22.dart
@@ -0,0 +1,6 @@
+void main() {
+  print(
+      'Q.22: Given a map representing a shopping cart with keys as product names and values as quantities, write Dart code to check if a product named "Apple" exists in the cart. Print "Product found" if it exists, otherwise print "Product not found".');
+  
+
+}
