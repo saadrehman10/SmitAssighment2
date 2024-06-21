2516f642f87ccf8324c5cc3b984900b164ef9439
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-21 20:03:31 +0500

    update

--- task16.dart
+++ task16.dart
@@ -1,4 +1,33 @@
 void main() {
   print(
       "Q.16: Implement a Dart code that uses the where() method to filter out odd numbers from a list of integers. The program should take in the original list as a parameter and print a new list containing only the even numbers.");
+  List<int> evenAndOddList = [
+    2,
+    5,
+    8,
+    11,
+    14,
+    17,
+    20,
+    23,
+    26,
+    29,
+    32,
+    35,
+    38,
+    41,
+    44,
+    47,
+    50,
+    53,
+    56,
+    59
+  ];
+
+  print('Even and odd list: $evenAndOddList');
+
+  List<int> evenList =
+      evenAndOddList.where((element) => element % 2 == 0).toList();
+
+  print('Even list $evenList');
 }
