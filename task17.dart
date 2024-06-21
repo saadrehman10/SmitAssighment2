83c5537c1f33c918ce5acf41f0b15a5d6926d3a0
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-21 23:53:09 +0500

    update

--- task17.dart
+++ task17.dart
@@ -1,4 +1,27 @@
 void main() {
   print(
       'Q.17: Given a list of integers, write a Dart code that uses the map() method to  create a new list with each value squared. The program should take in the original list as a parameter and print the new list.');
+List<int> array = [
+    2,
+    5,
+    8,
+    11,
+    14,
+    17,
+    20,
+    23,
+    26,
+    29,
+    32,
+    35,
+    38,
+    41,
+    44,
+    47,
+    50,
+    53,
+    56,
+    59
+  ];
+ 
 }
